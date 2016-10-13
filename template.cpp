00dfba88cb54c5576c1dba791bcdb8207f08dfda
TinyTiny <980821718@qq.com>
2016-10-14 00:58:56 +0800

    create repo

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+
+
+#define pii pair<int, int>
+#define mp make_pair<int, int>
+
+typedef long long ll;
+
+
+
+int main()
+{
+	
+	printf("\n");
+	return 0;
+}
